1a0fe41289e708814680abad810c9432257e81c3
KaramSahoo <51832871+KaramSahoo@users.noreply.github.com>
2020-03-09 18:34:44 +0530

    Update SpaceInvader.py

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -2,8 +2,6 @@ import pygame
 pygame.init()
 
 win = pygame.display.set_mode((800,600))
-pygame.display.set_caption("DEMO")
-
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("alien.png")
 pygame.display.set_icon(icon)
@@ -11,9 +9,10 @@ pygame.display.set_icon(icon)
 spaceshipImg = pygame.image.load("spaceship.png")
 spaceshipX = 368
 spaceshipY = 480
+spaceshipX_change = 0
 
-def spaceship():
-	win.blit(spaceshipImg, (spaceshipX , spaceshipY))
+def spaceship(x, y):
+	win.blit(spaceshipImg, (x, y))
 
 
 running = True
@@ -22,6 +21,16 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-	
-	spaceship()
+		
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				spaceshipX_change = -0.3
+			if event.key == pygame.K_RIGHT:
+				spaceshipX_change = 0.3
+		if event.type == pygame.KEYUP:
+			if event.type == pygame.K_RIGHT or event.type == pygame.K_LEFT:
+				spaceshipX_change = 0
+
+	spaceshipX += spaceshipX_change
+	spaceship(spaceshipX, spaceshipY)
 	pygame.display.update()
