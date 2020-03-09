b4c728b44d7597fe2ebcf1a7da56eeec8c08b11a
KaramSahoo <51832871+KaramSahoo@users.noreply.github.com>
2020-03-09 14:45:35 +0530

    Update SpaceInvader.py
    Changed the text, added an icon for the game window and set a background color of rgb code 000.

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -4,8 +4,14 @@ pygame.init()
 win = pygame.display.set_mode((700,500))
 pygame.display.set_caption("DEMO")
 
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("alien.png")
+pygame.display.set_icon(icon)
+
 running = True
 while running:
+	screen.fill((0, 0, 0))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+	pygame.display.update()
